3b8b8fd10b3ce9683771335af9dcb8807c4e6ff3
Stefan Mehnert <StefanMehnert@gmail.com>
2016-07-17 19:13:36 +0200

    Initial Commit

---
+++ SleepBabySleep/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SleepBabySleep
+//
+//  Created by Stefan Mehnert on 17/07/16.
+//  Copyright © 2016 Stefan Mehnert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
