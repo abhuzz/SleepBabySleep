f1e53e2ae111fd20ee3a0b9f372395eb1185d5d9
Stefan Mehnert <StefanMehnert@gmail.com>
2016-08-29 20:16:03 +0200

    Removed comment in appDelegate, set NSMicrophoneUsageDescription in info.plist

--- SleepBabySleep/AppDelegate.swift
+++ SleepBabySleep/AppDelegate.swift
@@ -19,7 +19,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
-        // Set AVAudioSession Category
         let session = AVAudioSession.sharedInstance()
         
         do {
