0dc0eb432df785f33c1eef4039e270adabac7902
Stefan Mehnert <StefanMehnert@gmail.com>
2016-08-31 16:50:06 +0200

    Added a first audioRecorder implementation

---
+++ SleepBabySleep/AudioRecorder.swift
@@ -0,0 +1,55 @@
+//
+//  AudioRecorder.swift
+//  SleepBabySleep
+//
+//  Created by Stefan Mehnert on 31/08/16.
+//  Copyright © 2016 Stefan Mehnert. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+class AudioRecorder: NSObject { // for AVAudioRecorderDelegate :-(
+    
+    var audioRecorder: AVAudioRecorder!
+    
+    
+    init(fileURL: NSURL) {
+        
+        super.init()
+        
+        let recordSettings = [
+            AVFormatIDKey: Int(kAudioFormatLinearPCM),
+            AVSampleRateKey: 44100.0,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.High.rawValue
+        ]
+        
+        do {
+            audioRecorder = try AVAudioRecorder(URL: fileURL, settings: recordSettings as! [String : AnyObject])
+            audioRecorder.delegate = self
+            audioRecorder.prepareToRecord()
+        } catch {
+            NSLog("Error creating audioRecorder.")
+        }
+    }
+    
+    func start() {
+        audioRecorder.record()
+    }
+    
+    func stop() {
+        audioRecorder.stop()
+    }
+    
+    func temporaryRecordingURL() -> NSURL {
+        return NSURL.fileURLWithPath("\(NSTemporaryDirectory())/TmpRecording.caf")
+    }
+}
+
+extension AudioRecorder: AVAudioRecorderDelegate {
+    
+    func audioRecorderDidFinishRecording(recorder: AVAudioRecorder, successfully flag: Bool) {
+        
+    }
+}
