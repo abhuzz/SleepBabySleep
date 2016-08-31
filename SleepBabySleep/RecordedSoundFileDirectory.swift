67a035626541aebf9a1ea8c3def8935519d3c852
Stefan Mehnert <StefanMehnert@gmail.com>
2016-08-31 20:48:34 +0200

    Added RecordedSoundFileDirectory class which will liste the recorded files from the documents directory

---
+++ SleepBabySleep/RecordedSoundFileDirectory.swift
@@ -0,0 +1,9 @@
+//
+//  RecordedSoundFileDirectory.swift
+//  SleepBabySleep
+//
+//  Created by Stefan Mehnert on 31/08/16.
+//  Copyright © 2016 Stefan Mehnert. All rights reserved.
+//
+
+import Foundation
