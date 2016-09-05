05af494abb76d763bd3f25cd2f92a4a8933a0cf0
Stefan Mehnert <StefanMehnert@gmail.com>
2016-09-05 05:34:04 +0200

    Changed audioSession options: enabled bluetooth, duck other apps playing audio

--- SleepBabySleep/AppDelegate.swift
+++ SleepBabySleep/AppDelegate.swift
@@ -22,7 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let session = AVAudioSession.sharedInstance()
         
         do {
-            try session.setCategory(AVAudioSessionCategoryPlayAndRecord, withOptions: AVAudioSessionCategoryOptions.DefaultToSpeaker)
+            try session.setCategory(AVAudioSessionCategoryPlayAndRecord, withOptions: [.AllowBluetooth, .DuckOthers])
             
             try session.setActive(true)
             
