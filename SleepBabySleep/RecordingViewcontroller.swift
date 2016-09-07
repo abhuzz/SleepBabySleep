428c837901eb65deee6ffc70d0ea24bb9685df5f
Stefan Mehnert <StefanMehnert@gmail.com>
2016-09-07 19:11:14 +0200

    Added new emty viewcontroller RecordingViewcontroller

---
+++ SleepBabySleep/RecordingViewcontroller.swift
@@ -0,0 +1,13 @@
+//
+//  RecordingViewcontroller.swift
+//  SleepBabySleep
+//
+//  Created by Stefan Mehnert on 07/09/16.
+//  Copyright © 2016 Stefan Mehnert. All rights reserved.
+//
+
+import UIKit
+
+class RecordingViewcontroller: UIViewController {
+    
+}
