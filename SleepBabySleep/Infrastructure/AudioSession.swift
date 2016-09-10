fe51721928a97e18fb9e173fa5d2979a1de72cc2
Stefan Mehnert <StefanMehnert@gmail.com>
2016-09-10 11:21:38 +0200

    Added protocol AudioSession with implementation AVAudioSessionFacade

---
+++ SleepBabySleep/Infrastructure/AudioSession.swift
@@ -0,0 +1,46 @@
+//
+//  AudioSession.swift
+//  SleepBabySleep
+//
+//  Created by Stefan Mehnert on 10/09/16.
+//  Copyright © 2016 Stefan Mehnert. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+protocol AudioSession {
+    func openForPlayback() throws
+    func openForRecording() throws
+    func close() throws
+}
+
+class AVAudioSessionFacade: AudioSession {
+    
+    private let session = AVAudioSession.sharedInstance()
+    
+    private(set) var microphoneAvailable = false
+    
+    
+    func openForPlayback() throws {
+     
+        try session.setCategory(AVAudioSessionCategoryPlayback, withOptions: [])
+        try session.setActive(true)
+    }
+    
+    func openForRecording() throws {
+        
+        try session.setCategory(AVAudioSessionCategoryPlayAndRecord, withOptions: [.DefaultToSpeaker, .AllowBluetooth])
+        try session.setActive(true)
+        
+        session.requestRecordPermission({(granted: Bool)-> Void in
+            self.microphoneAvailable = granted
+        })
+
+    }
+    
+    func close() throws {
+        
+        try session.setActive(false)
+    }
+}
